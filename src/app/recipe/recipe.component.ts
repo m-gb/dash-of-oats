d305702d1ad27ab505038b7fd19b4b2e85af7253
m-gb <maya.goldberg90@gmail.com>
2019-03-16 23:41:04 +0200

    Add page to display individual recipes

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IRecipe } from '../../../api/src/interfaces/recipe';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe.component.scss']
 })
 export class RecipeComponent implements OnInit {
+  recipe: IRecipe;
 
-  constructor() { }
+  constructor(private rs: RecipeService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.rs.getRecipe(params.get('name')).subscribe((data: IRecipe) => {
+        this.recipe = data;
+      });
+    });
   }
 
+  editName(name: string): string {
+    return this.rs.editName(name);
+  }
 }
