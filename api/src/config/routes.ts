f1dbed4e967f9ed54edeaa8224a9687910e2ba92
m-gb <maya.goldberg90@gmail.com>
2019-03-16 23:35:32 +0200

    Update recipe routing

--- api/src/config/routes.ts
+++ api/src/config/routes.ts
@@ -13,3 +13,14 @@ router.route('/api/v1/recipes').get((req, res) => {
     }
   })
 })
+
+router.route('/api/v1/recipes/:name').get((req, res) => {
+  Recipe.findOne({ name: req.params.name }, (err: any, recipe: IRecipeModel) => {
+    if (err) {
+      res.status(500).json({ error: err.message })
+    }
+    else {
+      res.status(200).json(recipe)
+    }
+  })
+})
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'recipes/:name', component: RecipeComponent }
 ];
 
 @NgModule({
