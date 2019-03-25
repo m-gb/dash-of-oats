0b2982a248aa5ad25f957b025fdbb85388610244
m-gb <maya.goldberg90@gmail.com>
2019-03-25 18:06:02 +0200

    Update api tests

--- api/src/test/recipe.spec.ts
+++ api/src/test/recipe.spec.ts
@@ -15,35 +15,21 @@ describe('Recipe API', () => {
     return chai.request(app).get('/api/v1/recipes/banana_pancakes').then(res => {
       chai.expect(res.status).to.equal(200)
       chai.expect(res).to.be.json
-      chai.expect(res.text).not.to.eql("null")
+      chai.expect(res.text).not.null
+    })
+  })
+  it('/categories should return json', () => {
+    return chai.request(app).get('/api/v1/categories').then(res => {
+      chai.expect(res.status).to.equal(200)
+      chai.expect(res).to.be.json
+      chai.expect(res.text).not.null
     })
   })
   it('/categories/breakfasts should return json', () => {
     return chai.request(app).get('/api/v1/categories/breakfasts').then(res => {
       chai.expect(res.status).to.equal(200)
       chai.expect(res).to.be.json
-      chai.expect(res.text).not.to.eql("null")
-    })
-  })
-  it('/categories/desserts should return json', () => {
-    return chai.request(app).get('/api/v1/categories/desserts').then(res => {
-      chai.expect(res.status).to.equal(200)
-      chai.expect(res).to.be.json
-      chai.expect(res.text).not.to.eql("null")
-    })
-  })
-  it('/categories/lunches should return json', () => {
-    return chai.request(app).get('/api/v1/categories/lunches').then(res => {
-      chai.expect(res.status).to.equal(200)
-      chai.expect(res).to.be.json
-      chai.expect(res.text).not.to.eql("null")
-    })
-  })
-  it('/categories/sauces should return json', () => {
-    return chai.request(app).get('/api/v1/categories/sauces').then(res => {
-      chai.expect(res.status).to.equal(200)
-      chai.expect(res).to.be.json
-      chai.expect(res.text).not.to.eql("null")
+      chai.expect(res.text).not.null
     })
   })
 })
